be13664ce0d4e8b18ae7d5e316d1f0d60c200e17
sadmin <sadmin@PC373344.cts.com>
2017-10-07 14:26:33 +0530

    Pushing pivotal-testapp draft version -SA

---
+++ src/main/java/com/cts/pivotal/pivotaltestapp/resource/PivotalTestAppResource.java
@@ -0,0 +1,17 @@
+package com.cts.pivotal.pivotaltestapp.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/testapp")
+public class PivotalTestAppResource {
+
+	@GetMapping("/{username}")
+	public String sayHello(@PathVariable String username) {
+		return "Hello " + username + ", welcome to Pivotal Cloud Foundry!";
+	}
+
+}
